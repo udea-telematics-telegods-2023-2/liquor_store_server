2a6642eab2ceccdfb33ce29942faf934de8c8e5e
César Montoya <cesara.montoya1@udea.edu.co>
2023-11-25 19:04:46 -0500

    [Style] Formatted files and added some suggestions
    Signed-off-by: César Montoya <cesara.montoya1@udea.edu.co>

--- src/liquor.py
+++ src/liquor.py
@@ -1,32 +1,33 @@
-from argon2 import PasswordHasher
-from argon2.exceptions import VerifyMismatchError
-from src.db import Liquor, LiquorDatabase
-from uuid import uuid4
+from src.db import LiquorDatabase
 import json
 
+
 class Store:
+    # FIX ME: falta documentación estilo Google de la clase, y de cada uno de sus funciones
     def __init__(self, database: LiquorDatabase = LiquorDatabase()):
         self.__database = database
-    def check_liquor(self, uuid: str = "") -> tuple[int,str]:
-        if uuid == "": #si dejan el espacio de nombre vacio
+
+    def check_liquor(self, uuid: str = "") -> tuple[int, str]:
+        if uuid == "":  # si dejan el espacio de nombre vacio
             return 253, ""
-        free_liquor_name = self.__database.read(uuid=uuid) 
-        if not free_liquor_name:  #si no se encuentra el licor en la tienda      
+        # CHANGE: cambié el nombre de free_liquor_name a liquor para
+        # hacer referencia a un licor de la base de datos
+        liquor = self.__database.read(uuid=uuid)
+        # CHANGE: cambié la comparación para que sea explícita
+        # (comparar si es del tipo None)
+        # si no se encuentra el licor en la tienda
+        if liquor is None:
             return 252, ""
-        liquor_data = free_liquor_name.get_data()
-        if liquor_data[3] == 0: #si no hay stock del licor pedido
-            return 253
-        if free_liquor_name:
-            return 0, "ok"
-    def list(self) -> tuple[int,str]: 
-        free_liquors_name = self.__database.read_all() 
-        list_liquors_name = json.dumps(list_liquors_name)
+        liquor_data = liquor.get_data()
+        # si no hay stock del licor pedido
+        if liquor_data[3] == 0:
+            return 253, ""  # FIX ME: 253 NO ES ERROR DE STOCK
+        # si todo sale bien, retornar error 0
+        return 0, ""
 
-        #restored_list_liquors = json.loads(list_liquors_name)
-
+    def list(self) -> tuple[int, str]:
+        liquors = self.__database.read_all()
+        list_liquors_name = json.dumps(liquors)
+        # FIX ME: Borrar este comentario y todos los que puse que son reseñas
+        # restored_list_liquors = json.loads(list_liquors_name)
         return 0, list_liquors_name
-
-
-
-
-
--- src/server.py
+++ src/server.py
@@ -1,51 +1,63 @@
+#!/usr/bin/env python
+import json
 import logging
-from rsa import key, encrypt, decrypt, newkeys
-from socketserver import ForkingTCPServer, BaseRequestHandler
-from socketserver import UDPServer, BaseRequestHandler
+from socket import AF_INET, SOCK_DGRAM, socket
+from socketserver import ForkingTCPServer, ForkingUDPServer, BaseRequestHandler
 from src.liquor import Store
 from sys import argv, exit
-import json
-from socket import AF_INET, SOCK_DGRAM, socket
+from random import randint
+from threading import Thread
+
+
 class User_store:
-    #se verificará si hay un usuario conectado
-    #en este caso name tendrá que ser el uuid
+    # se verificará si hay un usuario conectado
+    # en este caso name tendrá que ser el uuid
     def __init__(self, name):
         self.name = name
         self.conect = False
-    #si el usuario está conectado se retornará el nombre de este para luego 
-    #enviarlo al banco
+
+    # si el usuario está conectado se retornará el nombre de este para luego
+    # enviarlo al banco
     def conectar(self):
         print(f"{self.name} se ha conectado.")
         self.conect = True
         return self.name
+
     def disconect(self):
         print(f"{self.name} se ha desconectado.")
         self.conect = False
-    #consultar estado de conexión:
+
+    # consultar estado de conexión:
     def is_conect(self):
         return self.conect
 
     def show_list(self, liquor_list):
-        if self.conect:           
+        if self.conect:
             print(f"Lista de licores:\n{liquor_list}")
+
     def chose_one(self, liquor_list, opcion):
         if self.is_conect():
             try:
                 opcion = int(opcion)
                 liquor_list2 = json.loads(liquor_list)
                 if 1 <= opcion <= len(liquor_list2):
-                    print(f"{self.name} seleccionó el licor: {liquor_list2[opcion - 1]}")
+                    print(
+                        f"{self.name} seleccionó el licor: {liquor_list2[opcion - 1]}"
+                    )
                 else:
                     print("No valid opcion.")
             except ValueError:
                 print("Por favor, ingrese un número válido.")
-    
+
+
 class LiquorDelivery:
     def __init__(self, user_name):
         self.user_name = user_name
+
     def get_virtual_liquor(self, liquor_name):
         # Lógica para obtener el licor virtual correspondiente al nombre
         return f"Virtual {liquor_name}"
+
     def deliver_liquor(self, liquor_name):
         # Simular la validación del pago
         payment_validation = self.validate_payment()
@@ -57,17 +69,21 @@ class LiquorDelivery:
         else:
             print(f"Error en la validación del pago: {payment_validation_result}")
             return None
+
     def validate_payment(self):
         # confirmación "OK" desde el servidor del banco
         return "OK"
+
     def get_virtual_liquor(self, liquor_name):
         # obtener el licor virtual correspondiente al nombre
         return f"Virtual {liquor_name}"
+
+
 class conection_bank:
     def bank_conection_server(self, mensaje):
-        #dirección del servidor del banco
+        # dirección del servidor del banco
         server_bank_direction = ("127.0.0.0", 5555)
-        #UDP socket
+        # UDP socket
         with socket(AF_INET, SOCK_DGRAM) as udp_socket:
             udp_socket.sendto(mensaje.encode("utf-8"), server_bank_direction)
             print("Mensaje enviado al servidor del banco")
@@ -76,13 +92,16 @@ class conection_bank:
             print("Respuesta del servidor del banco:", response.decode("utf-8"))
 
             if response.decode("utf-8") == "OK":
-                print("La transacción fue exitosa. Entregar el licor virtual al usuario.")
-                #tomemos a Username como el uuid
+                print(
+                    "La transacción fue exitosa. Entregar el licor virtual al usuario."
+                )
+                # tomemos a Username como el uuid
                 user_name = "NombreUsuario"  # Reemplaza con el nombre de usuario real
                 liquor_name = "NombreLicor"  # Reemplaza con el nombre del licor real
                 liquor_delivery = LiquorDelivery(user_name)
                 liquor_delivery.deliver_liquor(liquor_name)
-                
+
+
 class MyHandler(BaseRequestHandler):
     def handle(self):
         print("Connection from ", str(self.client_address))
@@ -94,30 +113,26 @@ class MyHandler(BaseRequestHandler):
             _, user_uuid = decoded_data.split()
             # Aquí le pasamos a User_Store el uuid del cliente que se conectó
             connected_user = User_store(user_uuid)
-            # Aquí verificamos si está conectado 
+            # Aquí verificamos si está conectado
             connected_user.conectar()
             # Desplego la lista
             store = Store()
             _, liquor_list = store.list()
             # Se envía la lista al cliente
             conn.sendall(json.dumps(liquor_list).encode("utf-8"))
-            
+
             # Se recibe la elección del cliente
-            user_choice = conn.recv(4096).decode("utf-8")                              
+            user_choice = conn.recv(4096).decode("utf-8")
             # Obtener la opción del usuario y realizar la elección
             _, option = user_choice.split()
             connected_user.chose_one(liquor_list, option)
-            
+
             # Obtener el nombre del licor seleccionado
             selected_liquor_name = liquor_list[int(option) - 1]
-            
+
             # Obtener el número de puerto del socket
             server_port = conn.getsockname()[1]
             print(f"El número de puerto es: {server_port}")
 
         else:
             conn.sendall("Comando no reconocido".encode("utf-8"))
-
-
-
-    
